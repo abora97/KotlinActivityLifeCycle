5f4ebc98ad3f78002fc0be5a2fdca9afaf0d0bc6
Abora <https://github.com/abora97>
2020-02-24 01:13:33 +0200

    hello world !

---
+++ app/src/main/java/com/example/kotlinactivitylifecycle/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinactivitylifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this,"Hello World!",Toast.LENGTH_LONG).show()
+    }
+}
